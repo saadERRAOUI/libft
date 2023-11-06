a4e70f1f591f2ca815cc704b9e0d6064201ab1d9
saadERRAOUI <saaderraouiinfosup@gmail.com>
2023-11-06 11:46:22 +0100

    PUSH-1.1.2: uPDATE FT_MEMCHR.C - fix Leaks

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: serraoui <serraoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 11:41:43 by serraoui          #+#    #+#             */
-/*   Updated: 2023/11/05 16:21:21 by serraoui         ###   ########.fr       */
+/*   Updated: 2023/11/06 10:03:10 by serraoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,16 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*str;
-	int				i;
+	size_t			i;
 
 	str = (unsigned char *)s;
 	i = 0;
-	if (n <= 0)
-		return (NULL);
-	if ((char)c == '\0' && str)
-		return (str + ft_strlen((char *)str));
-	while (str[i] && i < n)
+	while (i < n)
 	{
-		if (str[i] == (unsigned char)c)
-			return (str + i);
+		if (*str == (unsigned char)c)
+			return (str);
+		str++;
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
