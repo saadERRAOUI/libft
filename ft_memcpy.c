c6c525f20c81c499ad1d1a608ac447355ea63758
saadERRAOUI <saaderraouiinfosup@gmail.com>
2023-11-06 18:39:48 +0100

    PUSH-1.1.7: Update ft_memcpy.c file

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: serraoui <serraoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 11:45:17 by serraoui          #+#    #+#             */
-/*   Updated: 2023/11/06 15:21:22 by serraoui         ###   ########.fr       */
+/*   Updated: 2023/11/06 18:37:24 by serraoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-	unsigned char	*d;
-	unsigned char	*s;
+	char	*d;
+	char	*s;
 
-	d = (unsigned char *)dst;
-	s = (unsigned char *)src;
+	d = (char *)dst;
+	s = (char *)src;
 	if (!s && !d)
 		return (NULL);
 	while (n--)
